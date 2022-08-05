a6d1d96c0d2540ba70d1731e136f0754dc44b676
Ayush Ray <ayushray38@gmail.com>
2022-08-06 00:54:06 +0530

    created app folder

--- Shoppers_Store/settings.py
+++ Shoppers_Store/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ran@ng^i2i=s!h%z8gg#1y=mxbky%(p(a=!lks13l81b-wq#+2'
+
+from decouple import config
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
