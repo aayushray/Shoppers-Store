cf7d1d6e3e4a138776c4f1dbb1597346c6f8c794
Ayush Ray <ayushray38@gmail.com>
2022-08-09 16:07:43 +0530

    Modified Home Page

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator,MinValueValidator
 
-from app.views import product_detail
+# from app.views import product_detail
 # Create your models here.
 
 STATE_CHOICES = (
