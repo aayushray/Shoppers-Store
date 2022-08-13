7d3e0ac370f85a06b93f67152bdb25dc6941fa70
Ayush Ray <ayushray38@gmail.com>
2022-08-13 22:41:54 +0530

    Added Forgot Password Feature

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from app import views
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
-from .forms import LoginForm, MyPasswordChangeForm
+from .forms import *
 urlpatterns = [
     path('',views.ProductView.as_view(),name="home"),
     path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name="product-detail"),
@@ -16,8 +16,15 @@ urlpatterns = [
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
     path('accounts/login',auth_views.LoginView.as_view(template_name = 'app/login.html',authentication_form=LoginForm),name='login'),
     path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
+    
     path('passwordchange/',auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
     path('passwordchangedone/',auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'),name='passwordchangedone'),
+    
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html',form_class=MySetPasswordForm),name='password_reset_confirm'),
+    path('password-reset-complete',auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'),name='password_reset_complete'),
+    
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
