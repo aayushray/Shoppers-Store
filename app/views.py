cf247cbc177832f7267c2d9775a6df820215b439
Ayush Ray <ayushray38@gmail.com>
2022-08-14 16:52:12 +0530

    Added increase quantity feature

--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ from django.views import View
 from app.models import Product
 from .forms import *
 from django.contrib import messages
+from django.db.models import Q
+from django.http import JsonResponse
 
 # def home(request):
 #  return render(request, 'app/home.html')
@@ -49,6 +51,28 @@ def show_cart(request):
             return render(request, 'app/addtocart.html',{'carts':cart,'total_amount':total_amount,'amount':amount,'shipping_amount':shipping_amount})
         else:
             return render(request,'app/emptycart.html')
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity+=1
+        c.save()
+        amount = 0.0
+        shipping_amount = 70.0
+        total_amount = 0.0
+        cart = Cart.objects.filter(user=request.user) 
+        for p in cart:
+            temp_amount = p.quantity * p.product.discounted_price
+            amount += temp_amount
+            total_amount = amount + shipping_amount
+
+            data = {
+                'quantity':c.quantity,
+                'amount':amount,
+                'total_amount':total_amount,
+                }
+        return JsonResponse(data)
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
